8c4c54a6c500d8dd291132bafe291d26bdd7f5ff
XAVIER MORENO LICERAS <xmorenli7.alumnes@ub052229.mat.ub.edu>
2015-06-03 16:08:13 +0200

    adding lib

--- exec_streming.sh
+++ exec_streming.sh
@@ -4,8 +4,8 @@ hadoop fs -rm -r output
 
 hadoop jar lib/hadoop-streaming-2.6.0.jar \
         -file mean/mapper.py    -mapper mean/mapper.py \
-        -file mean/reducer.py   -reducer mean/reducertmp.py \
-        -input input/2007.csv -output output \
+        -file mean/reducer.py   -reducer mean/reducer.py \
+        -input input -output output \
 	-numReduceTasks 1 
 
 rm -rf output/
